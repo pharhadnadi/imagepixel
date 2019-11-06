63700a7b71a7c7bcb9e9df6f08862064e477a654
pharhadnadi <34746277+pharhadnadi@users.noreply.github.com>
2019-11-06 09:11:16 +0330

    Update pythonimage.py

--- pythonimage.py
+++ pythonimage.py
@@ -1,6 +1,13 @@
+#Farhad Nadi 2019
 from PIL import Image
 import numpy as np
  
+
+numofpixels = 6 #there are 6 .png files named from 1 to 6
+chunklen = 5
+
+
+
 img = Image.open('lady0.jpg').convert('LA')
 #pix = img.load()
 
@@ -9,21 +16,19 @@ width, height = img.size
 
 newim = Image.new("RGB", (width, height), "white")
 
-chunklen = 5
+
 
 for row in range(0,width, chunklen):
     
     for col in range(0, height, chunklen):        
-       chuncksum=0
-    #    print(row, col)
+       chuncksum=0    
+       
        for rowchunck in range(1,chunklen):            
-           for colchunck in range(1,chunklen):        
-            #    print('(',row+rowchunck , col+colchunck,')', end=',')
-               
+           for colchunck in range(1,chunklen):                       
                if (row+rowchunck<width and col+colchunck<height):
                    chuncksum += img.getpixel((row+rowchunck, col+colchunck))[0]
        chuncksum/=(chunklen**2)
-       newpix = Image.open(str(6-(int(chuncksum/42.5)+1))+'.png')
+       newpix = Image.open(str(numofpixels-(int(chuncksum/(255/numofpixels))+1))+'.png')
        
 
        newim.paste(newpix.resize((chunklen, chunklen), Image.ANTIALIAS), (row,col))
